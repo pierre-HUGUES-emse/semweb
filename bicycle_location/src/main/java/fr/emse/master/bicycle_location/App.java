7b5dc9fbbe1e381c37920be1083e1ae41a39d331
sarahlaurax <sarah.laurax@gmail.com>
2019-10-25 14:06:13 +0200

    initialisation

---
+++ bicycle_location/src/main/java/fr/emse/master/bicycle_location/App.java
@@ -0,0 +1,13 @@
+package fr.emse.master.bicycle_location;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
